51fc492d4e3c3abbf4ddda7aee8ec7e8f91ca7b5
Akaczek <luk12349876@o2.pl>
2024-03-16 12:52:28 +0100

    Fix images

--- src/game/utils/loadAssets.ts
+++ src/game/utils/loadAssets.ts
@@ -1,11 +1,25 @@
+import clubCardsImg from '../assets/cards/clubs/club_cards.png';
+import clubCardsJson from '../assets/cards/clubs/club_cards.json';
+import diamondCardsImg from '../assets/cards/diamonds/diamond_cards.png';
+import diamondCardsJson from '../assets/cards/diamonds/diamond_cards.json';
+import heartCardsImg from '../assets/cards/hearts/heart_cards.png';
+import heartCardsJson from '../assets/cards/hearts/heart_cards.json';
+import spadeCardsImg from '../assets/cards/spades/spade_cards.png';
+import spadeCardsJson from '../assets/cards/spades/spade_cards.json';
+import bgImg from '../assets/casinotable.png';
+import cardBackImg from '../assets/card_back.png';
+import showCardsPowerupImg from '../assets/powerups/showCards.png';
+import raiseMinePowerupImg from '../assets/powerups/riseMine.png';
+import lowerOpponentsPowerupImg from '../assets/powerups/lowerOpponent.png';
+
 export const loadAssets = (scene: Phaser.Scene) => {
-  scene.load.atlas('club_cards', '/src/game/assets/cards/clubs/club_cards.png', '/src/game/assets/cards/clubs/club_cards.json');
-  scene.load.atlas('diamond_cards', '/src/game/assets/cards/diamonds/diamond_cards.png', '/src/game/assets/cards/diamonds/diamond_cards.json');
-  scene.load.atlas('heart_cards', '/src/game/assets/cards/hearts/heart_cards.png', '/src/game/assets/cards/hearts/heart_cards.json');
-  scene.load.atlas('spade_cards', '/src/game/assets/cards/spades/spade_cards.png', '/src/game/assets/cards/spades/spade_cards.json');
-  scene.load.image('bg', '/src/game/assets/casinotable.png');
-  scene.load.image('card_back', '/src/game/assets/card_back.png');
-  scene.load.image('show_cards_powerup', '/src/game/assets/powerups/showCards.png');
-  scene.load.image('raise_mine_powerup', '/src/game/assets/powerups/riseMine.png');
-  scene.load.image('lower_opponents_powerup', '/src/game/assets/powerups/lowerOpponent.png');
+  scene.load.atlas('club_cards', clubCardsImg, clubCardsJson);
+  scene.load.atlas('diamond_cards', diamondCardsImg, diamondCardsJson);
+  scene.load.atlas('heart_cards', heartCardsImg, heartCardsJson);
+  scene.load.atlas('spade_cards', spadeCardsImg, spadeCardsJson);
+  scene.load.image('bg', bgImg);
+  scene.load.image('card_back', cardBackImg);
+  scene.load.image('show_cards_powerup', showCardsPowerupImg);
+  scene.load.image('raise_mine_powerup', raiseMinePowerupImg);
+  scene.load.image('lower_opponents_powerup', lowerOpponentsPowerupImg);
 };
